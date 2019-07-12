2a3cbabbc221467f352bcadc2558917b307c02cd
Fadejimi Adegbulugbe <adegbulugbefadejimi@gmail.com>
2019-07-12 10:21:24 -0400

    update

---
+++ src/main/java/com/nfl/shedule/nflscheduleservice/NflScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nfl.shedule.nflscheduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflScheduleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NflScheduleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nfl/shedule/nflscheduleservice/NflScheduleServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nfl.shedule.nflscheduleservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NflScheduleServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
